51b73011f1c2f053020581c3f94ef196559f6aa2
russianidiot <russianidiot.github@gmail.com>
2017-06-15 00:51:29 +0300

    ^.Tests/clone.sh,.Tests/url.txt

--- .Tests/clone.sh
+++ .Tests/clone.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 { set +x; } 2>/dev/null
 
-[ -e ~/.Tests ] && echo "SKIP: ~/.Tests/ EXISTS, continue" && exit 0
-echo "DEBUG: ~/.Tests/ NOT EXISTS, continue"
+path=~/.Tests
 [[ -z "$TESTS_URL" ]] && {
 	echo "DEBUG: \$TESTS_URL NOT DEFINED"
 	txt="${BASH_SOURCE[0]%/*}/url.txt"
 	TESTS_URL="$(set -x; cat "$txt")" || exit
 }
 
-( set -x; git clone --depth 1 "$TESTS_URL" ~/.Tests ) || exit
+( set -x; git clone --depth 1 "$TESTS_URL" "$path" ) || exit
