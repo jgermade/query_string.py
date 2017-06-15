7c289e89434235a94393708cf77f6c9496cff11d
russianidiot <russianidiot.github@gmail.com>
2017-06-15 04:57:41 +0300

    ^.Tests/clone.sh,.Tests/url.txt

--- .Tests/clone.sh
+++ .Tests/clone.sh
@@ -8,4 +8,6 @@ path=~/.Tests
 	TESTS_URL="$(set -x; cat "$txt")" || exit
 }
 
+# known-issues: ssh permissions with urls like 'git@github.com:owner/repo.git'
+# fix: https url - 'https://github.com/owner/repo.git' 
 ( set -x; git clone --depth 1 "$TESTS_URL" "$path" ) || exit
